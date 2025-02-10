d63b9b7dc1741784a7e8090077e24183566485af
zuhalcode <zuhalcode@gmail.com>
2025-02-10 11:18:46 +0700

    fix(server) : fix serverless function

--- src/dev.ts
+++ src/dev.ts
@@ -1,7 +1,32 @@
-import app from "./index";
+import express from "express";
+import authRouter from "./routes/api";
+import db from "./utils/db";
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+app.use(express.json());
+
+// Koneksi database
+async function init() {
+  try {
+    const dbResponse = await db();
+    console.log("Database connected:", dbResponse);
+
+    app.get("/", (req, res) => {
+      res
+        .status(200)
+        .json({ message: "Server is running locally", data: null });
+    });
+
+    app.use("/api/auth", authRouter);
+
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+}
+
+init();
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,27 @@
-import app from "./index";
+import express, { Request, Response } from "express";
 import serverless from "serverless-http";
+import authRouter from "./routes/api";
+import db from "./utils/db";
 
+const app = express();
+app.use(express.json());
+
+// Koneksi database
+async function connectDB() {
+  try {
+    const dbResponse = await db();
+    console.log("Database connected:", dbResponse);
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+}
+connectDB();
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Server is running", data: null });
+});
+app.use("/api/auth", authRouter);
+
+// Export serverless function
 export const handler = serverless(app);
