d3d5cb95a98b1b99504fe68b86e87d8ba52d4752
zuhalcode <zuhalcode@gmail.com>
2025-03-13 10:21:58 +0700

    fix (routes) : fix route method of /me

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post("/me", authMiddleware, authController.me);
+router.get("/me", authMiddleware, authController.me);
 router.post("/activation", authController.activation);
 
 export default router;
